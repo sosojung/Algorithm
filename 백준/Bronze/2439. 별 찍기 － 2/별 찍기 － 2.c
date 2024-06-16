e9ad76594e69e539b65fceeceaff85238747781b
문소정 <164326127+sosojung@users.noreply.github.com>
2024-06-16 22:14:14 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  int n;
+  scanf("%d", &n);
+  for(int i=1; i<=n; i++){
+    for(int j=i; j<n; j++){
+      printf(" ");
+    }
+  for(int k=1; k<=i; k++){
+    printf("*");
+  }
+    printf("\n");
+  }
+  return 0;
+}
