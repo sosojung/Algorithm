aeb6f44ca378175d4f04084398c91d2c1ec3624d
문소정 <164326127+sosojung@users.noreply.github.com>
2024-06-21 10:38:21 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("         ,r\'\"7");
+printf("\nr`-_   ,'  ,/");
+printf("\n \\. \". L_r\'");
+printf("\n   `~\\/");
+printf("\n      |");
+printf("\n      |");
+    return 0;
+}
