00599837cd6c8782704cf3dda8ea3dd51d1c5e1f
문소정 <164326127+sosojung@users.noreply.github.com>
2024-06-22 22:29:40 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    
+    char s[101];
+    int ans = 0, i = 0;
+    
+    scanf("%s", s);
+    
+    while(i < strlen(s)) {
+        if(s[i] == 'c') {
+            if(s[i + 1] == '=')
+                i++;
+            else if(s[i + 1] == '-')
+                i++;
+        }
+    else if(s[i] == 'd') {
+        if(s[i + 1] == '-')
+            i++;
+        else if(s[i + 1] == 'z' && s[i + 2] == '=')
+        i += 2;
+    }
+    else if(s[i] == 'l') {
+        if(s[i + 1] == 'j')
+            i++;
+    }
+    else if(s[i] == 'n') {
+        if(s[i + 1] == 'j')
+            i++;
+    }
+    else if(s[i] == 's') {
+        if(s[i + 1] == '=')
+            i++;
+    }
+    else if(s[i] == 'z') {
+        if(s[i + 1] == '=')
+            i++;
+    }
+    
+    ans++;
+    i++;
+    }
+    printf("%d", ans);
+    
+    return 0;
+}
