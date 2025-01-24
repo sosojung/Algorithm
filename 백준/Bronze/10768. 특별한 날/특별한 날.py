f85d0b99eafaa098e0ffe2f87470dfe3b315e84b
문소정 <sojungmoon6@gmail.com>
2025-01-24 23:03:23 +0900

    [Bronze IV] Title: 특별한 날, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.py
@@ -0,0 +1,8 @@
+m = int(input())
+d = int(input())
+if m > 2 : print("After")
+elif m < 2 : print("Before")
+elif m == 2 :
+    if d > 18 : print("After")
+    elif d < 18 : print("Before")
+    else : print("Special")
