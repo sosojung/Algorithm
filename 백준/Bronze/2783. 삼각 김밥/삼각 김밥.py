6fb1fc214ee2663329adb053bfb2a9a89c77f160
문소정 <sojungmoon6@gmail.com>
2025-02-12 03:19:07 +0900

    [Bronze III] Title: 삼각 김밥, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2783. 삼각 김밥/삼각 김밥.py
+++ 백준/Bronze/2783. 삼각 김밥/삼각 김밥.py
@@ -1,15 +1,10 @@
-x, y = map(int, input().split())
-y1 = 1000 / y
-x1 = x * y1
-
 min_p = []
-min_p.append(x1)
+x, y = map(int, input().split())
+min_p.append(x*(1000/y))
 
 n = int(input())
 for i in range(n):
     x, y = map(int, input().split())
-    y_i = 1000/y
-    x_i = x*y_i
-    min_p.append(x_i)
+    min_p.append(x*(1000/y))
 
 print(f"{min(min_p):.2f}")
