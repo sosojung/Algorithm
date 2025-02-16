14443373b48c4c8f28b24cfd92a48b0c6f71da10
문소정 <sojungmoon6@gmail.com>
2025-02-16 22:13:46 +0900

    [Bronze IV] Title: 유치원생 파댕이 돌보기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/30979. 유치원생 파댕이 돌보기/유치원생 파댕이 돌보기.py
+++ 백준/Bronze/30979. 유치원생 파댕이 돌보기/유치원생 파댕이 돌보기.py
@@ -1,11 +1,8 @@
 t = int(input())
-n = int(input())
-f = []
-while True:
-    f = list(map(int, input().split()))
-    if len(f) == n:
-        break
-if sum(f) >= t:
+input()
+f = sum(map(int, input().split()))
+
+if f >= t:
     print("Padaeng_i Happy")
 else:
     print("Padaeng_i Cry")
